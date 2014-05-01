fcaa296f8e0078a8b97c577886536ef0f082b3fb
Rahul Bhargava <rahulb@media.mit.edu>
2014-05-01 11:53:51 -0400

    updates for v0.71

--- mediameter/cliff.py
+++ mediameter/cliff.py
@@ -8,15 +8,20 @@ class Cliff():
 
     PATH_TO_CLIFF = "/CLIFF/parse/text"
 
+    JSON_PATH_TO_ABOUT_COUNTRIES = 'results.places.about.countries';
+
+    STATUS_OK = "ok"
+
     def __init__(self,host,port):
         self._log = logging.getLogger('cliff')
         self._url = host+":"+str(port)+Cliff.PATH_TO_CLIFF
+        self._log.info("Conneced to CLIFF at "+self._url)
 
     def query(self,text):
         payload = {'q':text}
         try:
-            r = requests.get(self._url, params=payload)
-            self._log.info(r.content)
+            r = requests.post(self._url, data=payload)
+            self._log.debug('CLIFF says '+r.content)
             return r.json()
         except requests.exceptions.RequestException as e:
             self._log.error("RequestException " + str(e))
