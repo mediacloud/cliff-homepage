dca3437b41fb209e2781a2748825ea6d341cd8e3
Rahul B <rahulbot@gmail.com>
2017-11-08 10:30:28 -0500

    migrated to media cloud namespace, fresh coat of UI paint, new cliff lib, setup for dokku deployment

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 CLIFF-homepage
 ==============
 
-A simple homepage for the CLIFF project
+A simple homepage for the CLIFF project.
+
+
+Install
+-------
+
+`pip install -r requirements.txt`
+
+Configure
+---------
+
+You have to define a `CLIFF_URL` environment variable for this to work: `CLIFF_URL=http://localhost:8080`
+
+Using
+-----
+
+This is set up to be run in a Heroku-style container via the `Procfile`.  If you want to run it yourself you
+can just start `run.sh` on the command line.
--- server.py
+++ server.py
@@ -1,34 +1,44 @@
-import os, sys, time, logging, ConfigParser, json
+import os
+import sys
+import logging
+import json
 from flask import Flask, render_template, request
-from mediameter.cliff import Cliff
+
+from cliff.api import Cliff
 
 MAX_CHARS = 250 	# limit the amount of text users can send in
 
 app = Flask(__name__)
 
 # setup logging
-logging.basicConfig(filename='/var/log/cliff-homepage.log',level=logging.DEBUG)
+logging.basicConfig(level=logging.WARN)
 log = logging.getLogger('cliff-homepage')
 log.info("---------------------------------------------------------------------------")
 
-# load config file
-current_dir = os.path.dirname(os.path.abspath(__file__))
-config = ConfigParser.ConfigParser()
-config.read(current_dir+'/cliff.config')
-cliff = Cliff( config.get('cliff','host'), config.get('cliff','port') )
+# set up the api client we will use
+if 'CLIFF_URL' in os.environ:
+    CLIFF_URL = os.environ['CLIFF_URL']
+else:
+    log.error("You must set a CLIFF_URL environment variable that points to your server's base url")
+    sys.exit()
+
+cliff = Cliff(CLIFF_URL)
+
 
 # render the homepage
 @app.route("/")
 def index():
     return render_template('home.html')
 
+
 # return json results from CLIFF 
 @app.route("/process",methods=['POST'])
 def geoparse():
     text = request.form['text']
-    demonyms = request.form['demonyms']=='true'
-    results = cliff.parseText(text[0:MAX_CHARS],demonyms)
+    demonyms = request.form['demonyms'] == 'true'
+    results = cliff.parse_text(text[0:MAX_CHARS], demonyms)
     return json.dumps(results)
+
 
 if __name__ == "__main__":
     app.debug = True
