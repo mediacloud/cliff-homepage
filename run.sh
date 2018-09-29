27d5a25c21c2dfc2549abecf39e768d762362ddf
Rahul B <rahulbot@gmail.com>
2018-09-29 17:11:25 -0400

    Upgrade to python 3, containerization cleanup, user-facing version

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
 gunicorn server:app
+
--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-import os
-import sys
 import logging
 import json
+import config
 from flask import Flask, render_template, request
 
 from cliff.api import Cliff
+
+VERSION = "2.2.0"
 
 MAX_CHARS = 250 	# limit the amount of text users can send in
 
@@ -12,23 +13,20 @@ app = Flask(__name__)
 
 # setup logging
 logging.basicConfig(level=logging.WARN)
-log = logging.getLogger('cliff-homepage')
+log = logging.getLogger(__file__)
 log.info("---------------------------------------------------------------------------")
 
-# set up the api client we will use
-if 'CLIFF_URL' in os.environ:
-    CLIFF_URL = os.environ['CLIFF_URL']
-else:
-    log.error("You must set a CLIFF_URL environment variable that points to your server's base url")
-    sys.exit()
+app_config = config.get_default_config()
 
+# set up the api client we will use
+CLIFF_URL = app_config.get('CLIFF_URL')
 cliff = Cliff(CLIFF_URL)
 
 
 # render the homepage
 @app.route("/")
 def index():
-    return render_template('home.html')
+    return render_template('home.html', version=VERSION)
 
 
 # return json results from CLIFF 
